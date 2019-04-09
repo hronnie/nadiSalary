a3f3d54e61f0d7924ad5af8b4997fca451fe1e71
Aron Harsfalvi <h.roni23@gmail.com>
2019-04-09 17:48:42 +0200

    Adds new calculation

--- src/views/salary/SalaryCalc.jsx
+++ src/views/salary/SalaryCalc.jsx
@@ -14,15 +14,26 @@ import {Formik, Field, Form, ErrorMessage} from 'formik';
 class SalaryCalc extends React.Component {
     constructor() {
         super();
-        this.calculateSalary = this.calculateSalary.bind(this);
-        this.calcHourlyRate = this.calcHourlyRate.bind(this);
+        // this.calculateSalary = this.calculateSalary.bind(this);
+        // this.calcHourlyRate = this.calcHourlyRate.bind(this);
         this.state = {
             noOfTreatments: 0,
-            sumSalary: 0,
+            sumIncome: 0,
             sumTreatments: 0,
             expenses: 0,
             salaryPart: 0,
             hourlyRate: 0,
+            banknote: {
+                fifty: 0,
+                hundred: 0,
+                twoHundred: 0,
+                fiveHundred: 0,
+                thousand: 0,
+                twoThousand: 0,
+                fiveThousand: 0,
+                tenThousand: 0,
+                twentyThousand: 0,
+            }
         }
     }
 
@@ -30,20 +41,33 @@ class SalaryCalc extends React.Component {
 
     }
 
-    calculateSalary(value) {
-        debugger;
-        console.log(this.state.noOfTreatments);
+
+    calcSumIncome(banknote) {
+        let sum = 0;
+        sum += banknote.fifty * 50;
+        sum += banknote.hundred * 100;
+        sum += banknote.twoHundred * 200;
+        sum += banknote.fiveHundred * 500;
+        sum += banknote.thousand * 1000;
+        sum += banknote.twoThousand * 2000;
+        sum += banknote.fiveThousand * 5000;
+        sum += banknote.tenThousand * 10000;
+        sum += banknote.twentyThousand * 20000;
+        return sum;
     }
 
-    calcHourlyRate() {
-        debugger;
-        return this.state.noOfTreatments * 2;
+    targetMapper(targetName) {
+        let banknoteName = 'banknote.' + targetName;
+        if (Number.isInteger(this.state[banknoteName])) {
+            return banknoteName;
+        }
+        return targetName;
     }
 
     onChange = e => {
-        this.setState({ [e.target.name]: e.target.value },
+        this.setState({ [this.targetMapper(e.target.name)]: e.target.value },
             () => {
-                this.setState({hourlyRate: this.state.noOfTreatments * 2})
+                this.setState({sumIncome: this.calcSumIncome(this.state.banknote)})
             })
     }
 
@@ -57,6 +81,75 @@ class SalaryCalc extends React.Component {
                                 <CardTitle tag="h4">Állítsd be az óraszámokat</CardTitle>
                             </CardHeader>
                             <CardBody>
+                                <Row>
+                                    <Col md={3}>
+                                        <input
+                                            type="number"
+                                            name="fifty"
+                                            onChange={this.onChange}
+                                            value={this.state.banknote.fifty}/>
+                                    </Col>
+                                    <Col md={3}>
+                                        <input
+                                            type="number"
+                                            name="fifty"
+                                            onChange={this.onChange}
+                                            value={this.state.banknote.hundred}/>
+                                    </Col>
+                                    <Col md={3}>
+                                        <input
+                                            type="number"
+                                            name="fifty"
+                                            onChange={this.onChange}
+                                            value={this.state.banknote.twoHundred}/>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col md={3}>
+                                        <input
+                                            type="number"
+                                            name="fifty"
+                                            onChange={this.onChange}
+                                            value={this.state.banknote.fiveHundred}/>
+                                    </Col>
+                                    <Col md={3}>
+                                        <input
+                                            type="number"
+                                            name="fifty"
+                                            onChange={this.onChange}
+                                            value={this.state.banknote.thousand}/>
+                                    </Col>
+                                    <Col md={3}>
+                                        <input
+                                            type="number"
+                                            name="fifty"
+                                            onChange={this.onChange}
+                                            value={this.state.banknote.twoThousand}/>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col md={3}>
+                                        <input
+                                            type="number"
+                                            name="fifty"
+                                            onChange={this.onChange}
+                                            value={this.state.banknote.fiveThousand}/>
+                                    </Col>
+                                    <Col md={3}>
+                                        <input
+                                            type="number"
+                                            name="fifty"
+                                            onChange={this.onChange}
+                                            value={this.state.banknote.tenThousand}/>
+                                    </Col>
+                                    <Col md={3}>
+                                        <input
+                                            type="number"
+                                            name="fifty"
+                                            onChange={this.onChange}
+                                            value={this.state.banknote.twentyThousand}/>
+                                    </Col>
+                                </Row>
                                 <Row>
                                     <Col>
                                         Áron
@@ -76,9 +169,9 @@ class SalaryCalc extends React.Component {
                                     <Col>
                                         <input
                                             type="number"
-                                            name="sumSalary"
+                                            name="sumIncome"
                                             onChange={this.onChange}
-                                            value={this.state.sumSalary}/>
+                                            value={this.state.sumIncome}/>
                                     </Col>
                                 </Row>
                                 <Row>
